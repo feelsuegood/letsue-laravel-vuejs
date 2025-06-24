2cf0e7684213f0c2fea45ca18c84acd956040cbb
feelsuegood <129813530+feelsuegood@users.noreply.github.com>
2025-06-24 19:12:02 +1000

    props: passing data to pages

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -8,7 +8,9 @@ class IndexController extends Controller
 {
     public function index()
     {
-        return inertia('index/Index');
+        return inertia('index/Index', [
+            'message' => 'Hello from Laravel'
+        ]);
     }
     public function show()
     {
--- resources/js/pages/index/Index.vue
+++ resources/js/pages/index/Index.vue
@@ -1,8 +1,12 @@
 <template>
     <h1>Index</h1>
     <Link href="/hello">Show Page</Link>
+    <div>The message is {{ message }}</div>
 </template>
 
 <script setup lang="ts">
 import { Link } from "@inertiajs/vue3";
+// defineProps(['message'])
+// specify the type: recommended
+defineProps({message: String,})
 </script>
