36a2f746a6e82c5ec4e48c360f3759080f91dc3b
feelsuegood <129813530+feelsuegood@users.noreply.github.com>
2025-07-01 21:45:34 +1000

    reactive data by using ref() - proxy object

--- resources/js/layouts/MainLayout.vue
+++ resources/js/layouts/MainLayout.vue
@@ -1,10 +1,18 @@
 <template>
 <Link href="/">Main Page</Link>&nbsp;
 <Link href="/hello">Show Page</Link>
+<div>This page with time {{ timer }}</div>
 <!-- you can add default value inside of slot tag -->
 <slot>Default</slot>
 </template>
 
 <script setup lang="ts">
 import { Link } from "@inertiajs/vue3";
+import { ref } from "vue"
+
+// ref create a proxy object
+const timer = ref(0)
+setInterval(() => {
+    timer.value++
+}, 1000);
 </script>
